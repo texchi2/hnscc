b48f6eae4b086a92fd2f4bb3a53226672e758c1d
texchi2 <texchi2@gmail.com>
2018-11-30 00:01:47 +0800

    github

--- main_marginS_free_HNSCC.R
+++ main_marginS_free_HNSCC.R
@@ -6,8 +6,9 @@
 
 # [2018/10/26] sync: pull and push with remote github (well done); Screens connect from iPad pro (ok)
 #  git config remote.origin.url git@github.com:texchi2/hnscc.git; steps: commit/pull/push and done (to sync by ssh without id/pw)
-# Happy Birthday [2018/11/07] resume from project of hnscc_git
-# (2018/11/08 happy Yang's LN: start to shrink)
+# Happy Birthday [2018/11/07] resume from project of hnscc_github
+# (2018/11/08 happy Yang's LN: start to shrink => still => ANA 1:620 suggested SLE?)
+# [2018/11/30] Teamviewer is better.
 
 # Tutorial: Survival analysis of TCGA patients integrating gene expression (RNASeq) data
 # https://www.biostars.org/p/153013/
