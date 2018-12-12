c4a2818843f1b269e2fbb6a72b8bb4066dc4d7f3
texchi2 <texchi2@gmail.com>
2018-12-12 14:27:59 +0000

    ssh

--- main_marginS_free_HNSCC.R
+++ main_marginS_free_HNSCC.R
@@ -6,7 +6,8 @@
 
 # [2018/10/26] sync: pull and push with remote github (well done); Screens connect from iPad pro (ok)
 #  git config remote.origin.url git@github.com:texchi2/hnscc.git; 
-#  =>steps: commit/pull/push and done (to sync by ssh without id/pw)
+# add ssh RSA key to GitHub forserver instance of Rstudio server
+# =>steps: commit/pull/push and done (to sync by ssh without id/pw)
 # Happy Birthday [2018/11/07] resume from project of hnscc_github
 # (2018/11/08 happy Yang's LN: start to shrink => still => ANA 1:620 suggested SLE?)
 # [2018/11/30] Teamviewer is better.
