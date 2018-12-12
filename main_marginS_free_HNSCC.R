ffb171b6bbe6a87424d0ab48158da4f0cccc6309
texchi2 <texchi2@gmail.com>
2018-12-12 22:31:10 +0800

    ssh

--- main_marginS_free_HNSCC.R
+++ main_marginS_free_HNSCC.R
@@ -10,7 +10,8 @@
 # $ git config --global user.email "texchi2@gmail.com"
 # $ git config --global user.name "texchi2"
 # $ git config remote.origin.url git@github.com:texchi2/hnscc.git
-# }
+# or without --global for IBM Watson Studio instance (sudo is not available)
+# }  
 #  =>steps: commit/pull/push and done (to sync by ssh without id/pw)
 # Happy Birthday [2018/11/07] resume from project of hnscc_github
 # (2018/11/08 happy Yang's LN: start to shrink => still => ANA 1:620 suggested SLE?)
