793469bab6cdf60547c204304f1e5c938e27816d
texchi2 <texchi2@gmail.com>
2018-12-12 14:36:35 +0000

    ssh IBM

--- main_marginS_free_HNSCC.R
+++ main_marginS_free_HNSCC.R
@@ -5,8 +5,12 @@
 # porting to HNSCC_Tex_survival since [2018/10/24] {3 levels:main, TCGA_HNSCC_[marginS/marginFree], cutofFinder_func.R}
 
 # [2018/10/26] sync: pull and push with remote github (well done); Screens connect from iPad pro (ok)
-#  git config remote.origin.url git@github.com:texchi2/hnscc.git; 
+# {
 # add ssh RSA key to GitHub forserver instance of Rstudio server
+# $ git config --global user.email "texchi2@gmail.com"
+# $ git config --global user.name "texchi2"
+# $ git config remote.origin.url git@github.com:texchi2/hnscc.git
+# }
 # =>steps: commit/pull/push and done (to sync by ssh without id/pw)
 # Happy Birthday [2018/11/07] resume from project of hnscc_github
 # (2018/11/08 happy Yang's LN: start to shrink => still => ANA 1:620 suggested SLE?)
