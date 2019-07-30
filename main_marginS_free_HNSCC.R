0ce0a6e64c8a1e184985d9f9ed94193d6d54b004
texchi2 <texchi2@gmail.com>
2019-07-30 11:32:10 +0000

    running tobacco

--- main_marginS_free_HNSCC.R
+++ main_marginS_free_HNSCC.R
@@ -1764,7 +1764,7 @@ right <- anti_join(package_must, package_list, by="Package")
 
 
 ## START:  ####
-# since [2019/06/06]
+# since [2019/06/06][2019/07/30] tobacco_exposure
 TCGA_cohort <- "HNSCC" # cancer type: LUAD or HNSCC, defined
 path_cohort <- "~/R/HNSCC_Tex_survival/hnscc_github" # under rstudio-server on GCP instance 4
 setwd(path_cohort) # set the working directory under rstudio-server on HNSCC, GCP
